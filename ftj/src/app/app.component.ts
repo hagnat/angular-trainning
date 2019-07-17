4825e0d7c7259b55869f132c6d64b9dec127cf40
Valdir Bruxel Junior <v.bruxeljunior@global.leaseweb.com>
2019-07-17 12:43:38 +0000

    dockerfile, list available jewels

--- ftj/src/app/app.component.ts
+++ ftj/src/app/app.component.ts
@@ -17,15 +17,29 @@ export class AppComponent {
     this.initGame();
   }
   initGame() {
-    this.jewels = ['ruby','diamond','agate','amber','aquamarine','amethyst','opal','tourmaline', 'emerald','onyx','pearl','sapphire'];
+    this.jewels = [
+      'ruby',
+      'diamond',
+      'agate',
+      'amber',
+      'aquamarine',
+      'amethyst',
+      'opal',
+      'tourmaline',
+      'emerald',
+      'onyx',
+      'pearl',
+      'sapphire'
+    ];
     this.howManyTries = 0;
 
     // EXERCISE 1:
     // - What if we add new jewel to jewels? fix it!
 
-    let jewelIndex = Math.floor((Math.random() * 12) + 1);
+    let jewelIndex = Math.floor(Math.random() * this.jewels.length);
     this.origJewel = this.jewels[jewelIndex];
-      // console.log(this.origJewel);
+    console.log(this.origJewel);
+
     this.findJewel = null;
     this.hinting = null;
   }
